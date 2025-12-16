4ea3a9c6315c4d871a5928dc7d78cae766860d11
mena <tsikiniainatsilavina@gmail.com>
2025-12-16 15:01:51 +0300

    added dataretriever

--- src/main/java/org/example/DataRetriever.java
+++ src/main/java/org/example/DataRetriever.java
@@ -52,3 +52,84 @@ public class DataRetriever {
         }
     }
 
+
+
+
+//    findIngredients
+
+    public List<Ingredient> findIngredients(int page, int size) {
+        String sql = "SELECT * FROM ingredient LIMIT ? OFFSET ?";
+        List<Ingredient> ingredients = new ArrayList<>();
+
+        try (Connection conn = DbConnection.getDBConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, size);
+            ps.setInt(2, page * size);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                ingredients.add(new Ingredient(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getDouble("price"),
+                        CategoryEnum.valueOf(rs.getString("category")),
+                        null
+                ));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ingredients;
+    }
+
+//    create ingredient
+public List<Ingredient> createIngredients(List<Ingredient> newIngredients) {
+    String checkSql = "SELECT COUNT(*) FROM ingredient WHERE name = ?";
+    String insertSql = """
+                INSERT INTO ingredient(name, price, category, id_dish)
+                VALUES (?, ?, ?, ?)
+                """;
+
+    try (Connection conn = DBConnection.getDBConnection()) {
+        conn.setAutoCommit(false);
+
+        try {
+            for (Ingredient ing : newIngredients) {
+                try (PreparedStatement check = conn.prepareStatement(checkSql)) {
+                    check.setString(1, ing.getName());
+                    ResultSet rs = check.executeQuery();
+                    rs.next();
+
+                    if (rs.getInt(1) > 0) {
+                        throw new RuntimeException("Ingredient already exists: " + ing.getName());
+                    }
+                }
+
+                try (PreparedStatement insert = conn.prepareStatement(insertSql)) {
+                    insert.setString(1, ing.getName());
+                    insert.setDouble(2, ing.getPrice());
+                    insert.setString(3, ing.getCategory().name());
+                    insert.setObject(4,
+                            ing.getDish() != null ? ing.getDish().getId() : null,
+                            Types.INTEGER
+                    );
+                    insert.executeUpdate();
+                }
+            }
+
+            conn.commit();
+            return newIngredients;
+
+        } catch (RuntimeException e) {
+            conn.rollback();
+            throw e;
+        }
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+}
+}
