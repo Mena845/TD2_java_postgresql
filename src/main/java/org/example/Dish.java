deea9ea1c141851338df5a12fc676c0cf2f29d6a
mena <tsikiniainatsilavina@gmail.com>
2025-12-18 15:05:39 +0300

    creation de la classe Dish et Ingredient

---
+++ src/main/java/org/example/Dish.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.util.List;
+
+public class Dish {
+
+    private int id;
+    private String name;
+    private DishTypeEnum dishType;
+    private List<Ingredient> ingredients;
+
+    public Dish(int id, String name, DishTypeEnum dishType) {
+        this.id = id;
+        this.name = name;
+        this.dishType = dishType;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Double getDishPrice() {
+        return ingredients.stream()
+                .mapToDouble(Ingredient::getPrice)
+                .sum();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DishTypeEnum getDishType() {
+        return dishType;
+    }
+
+    public void setDishType(DishTypeEnum dishType) {
+        this.dishType = dishType;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+}
---
+++ src/main/java/org/example/Ingredient.java
@@ -0,0 +1,71 @@
+package org.example;
+
+
+public class Ingredient {
+
+    private int id;
+    private String name;
+    private Double price;
+    private CategoryEnum category;
+    private Dish dish;
+
+    public Ingredient(int id, String name, Double price, CategoryEnum category, Dish dish) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.dish = dish;
+    }
+
+    public Ingredient(String name, Double price, CategoryEnum category, Dish dish) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.dish = dish;
+    }
+
+    public String getDishName() {
+        return dish != null ? dish.getName() : null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+}
+
