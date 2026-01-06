25ccd0f4f9a07ba3cbe4da8e53ef8a590c0dc230
mena <tsikiniainatsilavina@gmail.com>
2026-01-06 15:41:47 +0300

    ajout de la methode saveDish

--- src/main/java/org/example/DataRetriever.java
+++ src/main/java/org/example/DataRetriever.java
@@ -68,7 +68,7 @@ public class DataRetriever {
 
     public List<Ingredient> createIngredients (List<Ingredient> newIngredients){
         String checkingquery = "SELECT * FROM Ingredient WHERE name = ?";
-        String insertionquery = "INSERT INTO Ingredient (name, price , category , id_dish) VALUES (?, ? ,? ,?)";
+        String insertionquery = "INSERT INTO Ingredient  (name, price , category , id_dish) VALUES (?, ? ,? ,?)";
 
         try (Connection conn = DBConnection.getConnection()){
             conn.setAutoCommit(false);
@@ -100,5 +100,46 @@ public class DataRetriever {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public Dish saveDish(Dish dish){
+        String insertDIsh = "INSERT INTO Dish (name, price, category) VALUES (?,?) RETURNING dish_id";
+        String updateDish = "Update dish set name = ?, price = ?, dish_type = ? where id = ?";
+        String deleteLinks = "Delete from dish_ingredient where dish_id = ?";
+        String insertLink = "Insert into dish_ingredient(dish_is , ingredient_id) VALUES (?, ?)";
+        try (Connection conn = DBConnection.getConnection()){
+            conn.setAutoCommit(false);
+            try{ if (dish.getId()==null){
+                try (PreparedStatement ps = conn.prepareStatement(insertDIsh)){
+                    ps.setString(1,dish.getName());
+                    ps.setString(2,dish.getDishType().name());
+                    ResultSet rs = ps.executeQuery();
+                    rs.next();
+                    dish.setId(rs.getInt(1));
+                }
+            }else{ try (PreparedStatement ps = conn.prepareStatement(updateDish)){
+                ps.setString(1,dish.getName());
+                ps.setString(2,dish.getDishType().name())
+                ps.setInt(3,dish.getId());
+                ps.executeUpdate();
+            }try (PreparedStatement ps = conn.prepareStatement(deleteLinks)){
+                ps.setInt(1,dish.getId());
+                ps.executeUpdate()
+            }
+            }for (Ingredient ing : dish.getIngredients()){
+                try(PreparedStatement ps = conn.prepareStatement(insertLink)){
+                    ps.setInt(1 , dish.getId());
+                    ps.setInt(2,ing.getId());
+                    ps.executeUpdate()
+                }
+            } conn.commit();
+                return dish;
+            }catch (RuntimeException e){
+                conn.rollback();
+                throw e;
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
