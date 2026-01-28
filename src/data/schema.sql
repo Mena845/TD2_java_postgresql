0ee7abb2d9338fdb849f65defb2ac1adeb2f40e3
mena <tsikiniainatsilavina@gmail.com>
2026-01-28 15:13:28 +0300

    creation de la classe StockMouvement

--- src/data/schema.sql
+++ src/data/schema.sql
@@ -18,3 +18,12 @@ CREATE TABLE dish_ingredient (
                                  ingredient_id INTEGER REFERENCES ingredient(id),
                                  PRIMARY KEY (dish_id, ingredient_id)
 );
+
+CREATE table stock_mouvement (
+                            id SERIAL primary key ,
+                            id_ingredient INTEGER REFERENCES ingredient(id),
+                            quantity numeric not null ,
+                            mouvement_type varchar(10)not null , --IN | OUT
+                            unit unit_type,
+                            creation_datetime timestamp
+)
---
+++ src/main/java/org/example/StockMouvement.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.Serial;
+
+public class StockMouvement {
+  int id;
+  value StcokValue;
+  type MouvementTypeEnum;
+  Instant creationDateTime;
+
+}
